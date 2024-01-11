9fe5ee5a3a7f2ec27201fa52729cf123a067412d
Advaitmenon1106 <advaitmenon1106@gmail.com>
2024-01-11 11:23:46 +0530

    Change the way the ID is assigned to each grid, by decrementing it by 1 (zero-counting).

--- src/ui/GenerateGrid.js
+++ src/ui/GenerateGrid.js
@@ -1,13 +1,17 @@
+import { Ship } from "../logic/Ship";
+import { shipEvents } from "./PlaceShip";
+
 export default function generateGrid() {
   const gridOutline = document.createElement("div");
-  gridOutline.id = 'grid-outline';
+  gridOutline.id = "grid-outline";
 
   document.body.appendChild(gridOutline);
 
   for (let i = 0; i < 100; i++) {
-    const box = document.createElement('div');
-    box.className = 'grid-boxes';
+    const box = document.createElement("div");
+    box.className = "grid-boxes";
     gridOutline.appendChild(box);
-    box.id = i;
+    box.id = String(i + 1);
   }
+  shipEvents(new Ship(5, 0, "Destroyer"));
 }
