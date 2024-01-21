bb6bca1cefb0e9f442ff56b7d82cb02b5027a414
Advaitmenon1106 <advaitmenon1106@gmail.com>
2024-01-21 18:54:57 +0530

    Change event listeners to the new functions for placing and showing the ship positions.

--- src/ui/GenerateGrid.js
+++ src/ui/GenerateGrid.js
@@ -1,9 +1,13 @@
+import { Gameboard } from "../logic/GameMaster";
 import { Ship } from "../logic/Ship";
-import { shipEvents } from "./PlaceShip";
+import { displayAvailablePositions, hoverColourFade, placeShip } from "./ShipEvents";
 
 export default function generateGrid() {
   const gridOutline = document.createElement("div");
+  const gameboard = new Gameboard();
+  const ship = new Ship(5, 0, "Battleship");
   gridOutline.id = "grid-outline";
+  const orientation = document.getElementById("orientation").innerHTML;
 
   document.body.appendChild(gridOutline);
 
@@ -11,7 +15,20 @@ export default function generateGrid() {
     const box = document.createElement("div");
     box.className = "grid-boxes";
     gridOutline.appendChild(box);
-    box.id = String(i + 1);
+    box.id = String(i);
+    
+    box.addEventListener("mouseover", (event)=>displayAvailablePositions(event, ship, gameboard, orientation.toLowerCase()));
+    box.addEventListener("mouseleave", (event)=>hoverColourFade(event, gameboard, ship.len, orientation));
+    box.addEventListener("click", (event)=>placeShip(event, gameboard, ship, orientation));
   }
-  shipEvents(new Ship(5, 0, "Destroyer"));
+  
 }
+
+/* 
+To-do:-
+
+1. Make <some functions> that take in gameboard and a shipObj as a parameter
+2. These functions will be used for the event listeners applied on each grid
+3. Comparisions will be made by superimposing the grid and the respective gameboard, and not by only using the AI, as done previously
+
+*/
